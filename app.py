b8a51ef96d3e91752a0176c6ef4bed01c845686c
mini-me <jocelyn_761@hotmail.com>
2022-01-05 16:55:46 -0700

    changed options and images

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ from dash.dependencies import Input, Output, State
 
 ########### Define your variables ######
 
-myheading1='How to use callbacks'
-tabtitle = 'xkcd'
-list_of_options=['box plot', 'correlation', 'git commit', 'scatterplot']
-list_of_images=['outlier.png', 'correlation.png', 'gitcommit.jpg', 'scatterplot.png', 'good_code.png']
+myheading1='How to get to Mt. Assiniboine'
+tabtitle = 'Hiking Options - Assiniboine'
+list_of_options=['Via Bryant Creek And Assiniboine Pass', 'Via Wonder Pass And Bryant Creek', 'Via Sunshine Village']
+list_of_images=['Assiniboine_Pass.jpg', 'Marvel_Pass.jpg', 'Citadel_Pass.jpg']
 sourceurl = 'https://xkcd.com/'
 githublink = 'https://github.com/austinlasseter/dash-callbacks-radio'
 
@@ -32,9 +32,8 @@ app.layout = html.Div(children=[
                 {'label':list_of_options[0], 'value':list_of_images[0]},
                 {'label':list_of_options[1], 'value':list_of_images[1]},
                 {'label':list_of_options[2], 'value':list_of_images[2]},
-                {'label':list_of_options[3], 'value':list_of_images[3]},
                 ],
-        value=list_of_images[4],
+        value=list_of_images[3],
         ),
     html.Div(id='your_output_here', children=''),
     html.Br(),
